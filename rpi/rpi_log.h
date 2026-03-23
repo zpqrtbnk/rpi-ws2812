dde96a4717b85beb3bc3daa5f37b4c3966f927e6
Stephan <stephane.gay@hazelcast.com>
2026-03-23 16:00:55 +0100

    wip

--- rpi/rpi_log.h
+++ rpi/rpi_log.h
@@ -1,5 +1,5 @@
 #define LOG(...) if (verbose) printf(__VA_ARGS__)
-#define ERR(...) printf(stderr, __VA_ARGS__)
+#define ERR(...) fprintf(stderr, __VA_ARGS__)
 
 extern int verbose;
 void log_verbose(int v);
